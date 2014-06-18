73c7184c1ec88428732d6c4a8a9754936302478d
lerouju8 <lerouju8@f1a0bb74-0b14-4967-bdef-63563ff65581>
2014-06-18 12:25:13 +0000

    Ajout de getters/setters sur la géométrie des QVertexItem
    git-svn-id: svn+ssh://pianosa/var/svn/gem/trunk/GEM++@12 f1a0bb74-0b14-4967-bdef-63563ff65581

--- src/GEM++/Application/Widgets/QVertexItem.cpp
+++ src/GEM++/Application/Widgets/QVertexItem.cpp
@@ -40,3 +40,90 @@ void QVertexItem::select(bool selected) {
     c.setAlpha(selected ? 128 : 0);
     setBrush(QBrush(c));
 }
+
+void QVertexItem::setX(qreal x) {
+    if(x >= 0)
+        setRect(x, y(), width(), height());
+}
+
+void QVertexItem::setY(qreal y) {
+    if(y >= 0)
+        setRect(x(), y, width(), height());
+}
+
+void QVertexItem::setWidth(qreal width) {
+    if(x() >= 0)
+        setRect(x(), y(), width, height());
+}
+
+void QVertexItem::setHeight(qreal height) {
+    if(y() >= 0)
+        setRect(x(), y(), width(), height);
+}
+
+qreal QVertexItem::x() const {
+    return rect().x();
+}
+
+qreal QVertexItem::y() const {
+    return rect().y();
+}
+
+qreal QVertexItem::width() const {
+    return rect().width();
+}
+
+qreal QVertexItem::height() const {
+    return rect().height();
+}
+
+void QVertexItem::setLeft(qreal left) {
+    if(left >= 0) {
+        setWidth(width()+x()-left);
+        setX(left);
+    }
+}
+
+void QVertexItem::setRight(qreal right) {
+    setWidth(right-left());
+}
+
+void QVertexItem::setTop(qreal top) {
+    if(top >= 0) {
+        setHeight(height()+y()-top);
+        setY(top);
+    }
+}
+
+void QVertexItem::setBottom(qreal bottom) {
+    setHeight(bottom-top());
+}
+
+qreal QVertexItem::left() const {
+    return x();
+}
+
+qreal QVertexItem::right() const {
+    return x()+width();
+}
+
+qreal QVertexItem::top() const {
+    return y();
+}
+
+qreal QVertexItem::bottom() const {
+    return y()+height();
+}
+
+void QVertexItem::translate(QPointF t, QSizeF size) {
+    if(left()+t.x() >= 0 && right()+t.x() <= size.width() &&
+            top()+t.y() >= 0 && bottom()+t.y() <= size.height()) {
+        setX(x()+t.x());
+        setY(y()+t.y());
+    }
+}
+
+void QVertexItem::fit(QSizeF size) {
+    setWidth(qMin(width(), size.width() - x()));
+    setHeight(qMin(height(), size.height() - y()));
+}
--- src/GEM++/Application/Widgets/QVertexItem.h
+++ src/GEM++/Application/Widgets/QVertexItem.h
@@ -14,6 +14,28 @@ class DLL_EXPORT QVertexItem : public QGraphicsRectItem {
         void invertColor();
         void view(bool visible = true);
         void select(bool selected = true);
+
+        void setX(qreal x);
+        void setY(qreal y);
+        void setWidth(qreal width);
+        void setHeight(qreal height);
+        qreal x() const;
+        qreal y() const;
+        qreal width() const;
+        qreal height() const;
+
+        void setLeft(qreal left);
+        void setRight(qreal right);
+        void setTop(qreal top);
+        void setBottom(qreal bottom);
+        qreal left() const;
+        qreal right() const;
+        qreal top() const;
+        qreal bottom() const;
+
+        void translate(QPointF t, QSizeF size);
+
+        void fit(QSizeF size);
 };
 
 #endif /* __QVERTEXITEM_H__ */
