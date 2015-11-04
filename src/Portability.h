78b1fc3daecc6f050aa173734e0ed4ab4ee84cf0
Julien Lerouge <julien@lerouge.me>
2015-11-04 16:47:41 +0100

    Prepared GEM++ for Windows release, made a distinct release for GLPK plugin.

--- src/Portability.h
+++ src/Portability.h
@@ -4,12 +4,14 @@
 // Cross-platform compilation
 #if defined(WIN32) || defined(WIN64) // Windows platform
     #ifdef DLL_BUILD
-		#define DLL_EXPORT __declspec(dllexport)
-	#else
-		#define DLL_EXPORT
-	#endif
-	#define GETPID _getpid
-	#define INFINITY HUGE_VAL
+            #define DLL_EXPORT __declspec(dllexport)
+    #else
+            #define DLL_EXPORT
+    #endif
+    #define GETPID _getpid
+    #ifndef INFINITY
+        #define INFINITY HUGE_VAL
+    #endif
 #elif defined(LINUX) && (__GNUC__ >= 4) // Linux platform
 	#define DLL_EXPORT __attribute__ ((visibility ("default")))
 	#define GETPID getpid
